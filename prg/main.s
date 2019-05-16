7148106ad705ecc6bf89cb8e7036286a3662d906
Nicholas Flynt <thegamefreak0134@gmail.com>
2019-05-16 18:12:40 -0500

    Initialize sprite palette and PPU, draw a basic sprite for testing

--- prg/main.s
+++ prg/main.s
@@ -52,22 +52,76 @@ initialize_mmc3:
 
 initialize_palettes:
         ; TEST: Set the palettes up with a nice pink for everything
+
+        ; Backgrounds
         lda #$3F
         sta PPUADDR
         lda #$00
         sta PPUADDR
-
         lda #$25
         sta PPUDATA
+        lda #$15
+        sta PPUDATA
+        lda #$05
+        sta PPUDATA
+        lda #$0F
+        sta PPUDATA
+
+        ; Sprites
+        lda #$3F
+        sta PPUADDR
+        lda #$11
+        sta PPUADDR
+        lda #$34
+        sta PPUDATA
+        lda #$14
+        sta PPUDATA
+        lda #$04
+        sta PPUDATA
+
+        ; Reset PPUADDR to 0,0
         lda #$00
         sta PPUADDR
         sta PPUADDR
+
+        rts
+
+initialize_ppu:
+        ; enable NMI interrupts and 8x16 sprites
+        lda #$A0
+        sta PPUCTRL
+        ; enable rendering everywhere
+        lda #$1E
+        sta PPUMASK
+        rts
+
+demo_oam_init:
+        lda #30
+        sta $0200 ;sprite[0].Y
+        lda #01
+        sta $0201 ;sprite[0].Tile + Nametable
+        lda #$00
+        sta $0202 ;sprite[0].Palette + Attributes
+        lda #30
+        sta $0203 ;sprite[0].X
+
+        lda #30
+        sta $0204 ;sprite[1].Y
+        lda #01
+        sta $0205 ;sprite[1].Tile + Nametable
+        lda #$40
+        sta $0206 ;sprite[1].Palette + Attributes
+        lda #38
+        sta $0207 ;sprite[1].X
         rts
 
         .export start
 start:
         jsr initialize_mmc3
         jsr initialize_palettes
+        jsr initialize_ppu
+        jsr demo_oam_init
+
 
 loop_endlessly:
         jmp loop_endlessly
