e2fee999237a0770405beab3a57a4eaaa6411159
Nicholas Flynt <thegamefreak0134@gmail.com>
2019-06-27 15:10:31 -0500

    Add a new large testing map, and a testing tileset
    The testing map is 64x48, which is somewhat shy of the maximum
    room size at 3072 bytes, but large enough to test scrolling
    routines. It won't work properly with the current PoC tileset,
    and will need the tileset conversion scripts to be written before
    it can be used.

---
+++ art/tilesets/Skull Tiles.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Skull Tiles" tilewidth="16" tileheight="16" tilecount="18" columns="4">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../tiles/bricks_inner_bl.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../tiles/bricks_inner_br.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../tiles/bricks_inner_tl.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../tiles/bricks_inner_tr.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../tiles/bricks_outer_bl.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../tiles/bricks_outer_bm.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../tiles/bricks_outer_br.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../tiles/bricks_outer_mr.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../tiles/bricks_outer_tl.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../tiles/bricks_outer_tm.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="../tiles/bricks_outer_tr.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../tiles/bricks_outter_ml.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="../tiles/grass.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="../tiles/hole.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="../tiles/solid_index_0.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="../tiles/solid_index_3.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="../tiles/solid_index_2.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="../tiles/solid_index_1.png"/>
+ </tile>
+</tileset>
