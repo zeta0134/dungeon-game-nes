57796479261a770679b7825facdf7ba68a75162b
Nicholas Flynt <thegamefreak0134@gmail.com>
2020-06-30 22:45:33 -0500

    Reference implementation for a simple sprite animation
    The idea is to eventually spit this data out with some sort of tool,
    but for now I'm hand-editing the data for testing.

---
+++ prg/blobby.s
@@ -0,0 +1,33 @@
+        .setcpu "6502"
+        .include "sprites.inc"
+
+.scope PRGLAST_E000
+        .segment "PRGLAST_E000"
+        ;.org $e000
+
+blobby_anim_idle:
+        .word blobby_frames_idle
+        .byte $2
+
+blobby_frames_idle:
+        .word blobby_oam_idle1
+        .byte $04, $04, 20; oam length, mapper, delay frames
+        .word blobby_oam_idle2
+        .byte $04, $04, 40
+
+blobby_oam_idle1:
+        ;     Y-offset  Tile  Attr   X-offset
+        .byte 0,        0,    $00,    0
+        .byte 8,        1,    $00,    0
+        .byte 0,        0,    $40,    8
+        .byte 8,        1,    $40,    8
+
+blobby_oam_idle2:
+        ;     Y-offset  Tile  Attr    X-offset
+        .byte 0,        2,    $00,    0
+        .byte 8,        3,    $00,    0
+        .byte 0,        2,    $40,    8
+        .byte 8,        3,    $40,    8
+
+
+.endscope
