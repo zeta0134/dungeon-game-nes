fa5b6d2545cee5773f2a3c42b0a77aa292e29093
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-05-04 05:21:38 -0400

    Break map loading out into its own file

---
+++ prg/map.s
@@ -0,0 +1,67 @@
+        .setcpu "6502"
+        .include "scrolling.inc"
+        .include "word_util.inc"
+        .include "zeropage.inc"
+
+.scope PRGLAST_C000
+        .segment "PRGLAST_C000"
+        ;.org $e000
+
+.export load_map, load_tileset
+
+; Loads a map into PRG RAM buffer, in preparation for drawing and gameplay
+; maybe gameplay stuff
+; Inputs:
+;   R4: 16bit address of the map to load
+; Clobbers: R0 - R4
+
+.proc load_map
+TempHeight := R0
+TempWidth := R1
+DestAddr := R2
+SourceAddr := R4
+        ldy #$00 ; we'll use indirect mode, but without an offset
+        lda (SourceAddr),y
+        sta MapWidth
+        sta TempHeight
+        clc
+        ror
+        sta AttributeWidth
+        inc16 SourceAddr
+        lda (SourceAddr),y
+        sta MapHeight
+        sta TempWidth
+        inc16 SourceAddr
+        st16 DestAddr, (MapData)
+loop:
+        ; copy in one byte
+        lda (SourceAddr),y
+        sta (DestAddr),y
+        inc16 SourceAddr
+        inc16 DestAddr
+        ; check MapWidth
+        dec TempWidth
+        bne loop
+        ; reload MapWidth and check MapHeight
+        lda MapWidth
+        sta TempWidth
+        dec TempHeight
+        bne loop
+        ; all done
+        rts
+.endproc
+
+; Load a tilemap into memory; these always have a fixed size of 256 bytes
+; Inputs:
+;   R0: 16bit address of the map to load
+.proc load_tileset
+        ldy #$00
+loop:
+        lda (R0),y
+        sta TilesetData,y
+        iny
+        bne loop
+        rts
+.endproc
+
+.endscope
--- prg/scrolling.s
+++ prg/scrolling.s
@@ -14,7 +14,7 @@
 MapData: .res 4096
 AttributeData: .res 1024
 TilesetData: .res 256
-.export MapData
+.export MapData, AttributeData, TilesetData
         .zeropage
 ; Map dimensions
 MapWidth: .byte $00
@@ -49,7 +49,7 @@ CameraXTileCurrent: .byte $00
 CameraXScrollCurrent: .byte $00
 CameraYTileCurrent: .byte $00
 CameraYScrollCurrent: .byte $00
-.exportzp CameraXTileTarget, CameraXScrollTarget, CameraYTileTarget, CameraYScrollTarget, MapWidth, MapHeight, PpuYTileTarget
+.exportzp CameraXTileTarget, CameraXScrollTarget, CameraYTileTarget, CameraYScrollTarget, MapWidth, MapHeight, AttributeWidth, PpuYTileTarget
 CameraXTileTarget: .byte $00
 CameraXScrollTarget: .byte $00
 CameraYTileTarget: .byte $00
@@ -59,7 +59,7 @@ PpuYTileTarget: .byte $00
         .segment "PRGLAST_C000"
         ;.org $e000
 
-.export load_map, load_tileset, init_map, init_attributes, scroll_camera, render_initial_viewport
+.export init_map, init_attributes, scroll_camera, render_initial_viewport
 
 .macro incColumn addr
 .scope
@@ -275,61 +275,6 @@ no_overflow:
 done:
 .endscope
 .endmacro
-
-; Loads a map into PRG RAM buffer, in preparation for drawing and gameplay
-; maybe gameplay stuff
-; Inputs:
-;   R4: 16bit address of the map to load
-; Clobbers: R0 - R4
-
-.proc load_map
-TempHeight := R0
-TempWidth := R1
-DestAddr := R2
-SourceAddr := R4
-        ldy #$00 ; we'll use indirect mode, but without an offset
-        lda (SourceAddr),y
-        sta MapWidth
-        sta TempHeight
-        clc
-        ror
-        sta AttributeWidth
-        inc16 SourceAddr
-        lda (SourceAddr),y
-        sta MapHeight
-        sta TempWidth
-        inc16 SourceAddr
-        st16 DestAddr, (MapData)
-loop:
-        ; copy in one byte
-        lda (SourceAddr),y
-        sta (DestAddr),y
-        inc16 SourceAddr
-        inc16 DestAddr
-        ; check MapWidth
-        dec TempWidth
-        bne loop
-        ; reload MapWidth and check MapHeight
-        lda MapWidth
-        sta TempWidth
-        dec TempHeight
-        bne loop
-        ; all done
-        rts
-.endproc
-
-; Load a tilemap into memory; these always have a fixed size of 256 bytes
-; Inputs:
-;   R0: 16bit address of the map to load
-.proc load_tileset
-        ldy #$00
-loop:
-        lda (R0),y
-        sta TilesetData,y
-        iny
-        bne loop
-        rts
-.endproc
 
 ; Initializes the scroll registers for the currently loaded map
 
