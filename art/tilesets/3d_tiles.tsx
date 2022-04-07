5132803032b306c93159dcb9040b32eee0222e07
Nicholas Flynt <thegamefreak0134@gmail.com>
2022-04-07 17:49:53 -0400

    Add a test tilemap with a few 3D structures. Graphics only for now

--- art/tilesets/3d_tiles.tsx
+++ art/tilesets/3d_tiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="3d_tiles" tilewidth="16" tileheight="16" tilecount="3" columns="0">
+<tileset version="1.8" tiledversion="1.8.2" name="3d_tiles" tilewidth="16" tileheight="16" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="16" source="../tiles/3d_floor_0.png"/>
@@ -7,7 +7,22 @@
  <tile id="1">
   <image width="16" height="16" source="../tiles/3d_floor_1.png"/>
  </tile>
- <tile id="2">
+ <tile id="4">
+  <image width="16" height="16" source="../tiles/3d_floor_2.png"/>
+ </tile>
+ <tile id="5">
   <image width="16" height="16" source="../tiles/3d_wall.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../tiles/3d_floor_0_lrshadow.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../tiles/3d_floor_0_lshadow.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../tiles/3d_floor_0_rshadow.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../tiles/3d_floor_0_tshadow.png"/>
  </tile>
 </tileset>
