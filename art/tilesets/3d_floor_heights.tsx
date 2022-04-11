bb380e5e2cbc8a4dfde0ace0bf3c5cc4beda9260
zeta0134 <noreply@reploid.cafe>
2022-04-11 15:01:30 -0400

    More consistent attribute naming. Tile 0 has no attributes at all.

--- art/tilesets/3d_floor_heights.tsx
+++ art/tilesets/3d_floor_heights.tsx
@@ -2,120 +2,117 @@
 <tileset version="1.8" tiledversion="1.8.2" name="3d_floor_heights" tilewidth="16" tileheight="16" tilecount="17" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <properties>
-   <property name="is_surface" type="bool" value="false"/>
-  </properties>
   <image width="16" height="16" source="../technical/blank.png"/>
  </tile>
  <tile id="1">
   <properties>
    <property name="floor_height" type="int" value="0"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_0.png"/>
  </tile>
  <tile id="2">
   <properties>
    <property name="floor_height" type="int" value="1"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_1.png"/>
  </tile>
  <tile id="3">
   <properties>
    <property name="floor_height" type="int" value="2"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_2.png"/>
  </tile>
  <tile id="4">
   <properties>
    <property name="floor_height" type="int" value="3"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_3.png"/>
  </tile>
  <tile id="5">
   <properties>
    <property name="floor_height" type="int" value="4"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_4.png"/>
  </tile>
  <tile id="6">
   <properties>
    <property name="floor_height" type="int" value="5"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_5.png"/>
  </tile>
  <tile id="7">
   <properties>
    <property name="floor_height" type="int" value="6"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_6.png"/>
  </tile>
  <tile id="8">
   <properties>
    <property name="floor_height" type="int" value="7"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_7.png"/>
  </tile>
  <tile id="9">
   <properties>
    <property name="floor_height" type="int" value="8"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_8.png"/>
  </tile>
  <tile id="10">
   <properties>
    <property name="floor_height" type="int" value="9"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_9.png"/>
  </tile>
  <tile id="11">
   <properties>
    <property name="floor_height" type="int" value="10"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_10.png"/>
  </tile>
  <tile id="12">
   <properties>
    <property name="floor_height" type="int" value="11"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_11.png"/>
  </tile>
  <tile id="13">
   <properties>
    <property name="floor_height" type="int" value="12"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_12.png"/>
  </tile>
  <tile id="14">
   <properties>
    <property name="floor_height" type="int" value="13"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_13.png"/>
  </tile>
  <tile id="15">
   <properties>
    <property name="floor_height" type="int" value="14"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_14.png"/>
  </tile>
  <tile id="16">
   <properties>
    <property name="floor_height" type="int" value="15"/>
-   <property name="is_surface" type="bool" value="true"/>
+   <property name="is_floor" type="bool" value="true"/>
   </properties>
   <image width="16" height="16" source="../technical/floor_15.png"/>
  </tile>
