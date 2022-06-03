6050001b30c711bf7fb334c57f161015c60c61a5
Nicholas Flynt <thegamefreak0134@gmail.com>
2022-06-03 01:57:22 -0400

    Add in some technical tiles to represent entrances and exits

---
+++ art/tilesets/technical.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="technical" tilewidth="16" tileheight="16" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../technical/blank.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../technical/exit.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../technical/entrance_1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../technical/entrance_2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../technical/entrance_3.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../technical/entrance_4.png"/>
+ </tile>
+</tileset>
