ed7016861ed4a631e4919e62984e278ca75ec474
Nicholas Flynt <thegamefreak0134@gmail.com>
2022-06-09 18:58:58 -0400

    Add an "entities" tileset, and some blobs for the test map
    These aren't hooked up to anything yet, but fortunately extraneous
    objects are also ignored, so there's that

---
+++ art/tilesets/entities.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="entities" tilewidth="16" tileheight="16" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="entity">
+  <properties>
+   <property name="initial_state" value="blobby_init"/>
+  </properties>
+  <image width="16" height="16" source="../technical/entity_blobby.png"/>
+ </tile>
+</tileset>
