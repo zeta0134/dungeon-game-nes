cb5499d4e044cce02a92c4335a3aea434396a687
Nicholas Flynt <thegamefreak0134@gmail.com>
2022-08-14 19:01:40 -0400

    Cleanup the underwater test map, add a more purple-y palette
    Combined with blue emphasis this looks much better. In particular,
    avoid large regions of bright (low row) palette seems to be ideal,
    we can reserve those hues for entities and particles instead.
    
    Also noteworthy, we want to avoid large regions of solid color if
    at all possible, because that makes all the work that we're doing on
    that hblank distortion essentially vanish.

---
+++ art/tilesets/green_green_underwater/chr1_test_tiles_3d_bg2.tsx
@@ -0,0 +1,64 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<tileset version="1.8" tiledversion="1.8.2" name="test_tiles_3d_bg2" tilewidth="16" tileheight="16" tilecount="8" columns="0">
+  <grid orientation="orthagonal" width="1" height="1" />
+  <properties>
+    <property name="chr1_tileset" type="string" value="test_tiles_3d_tileset" />
+    <property name="global_palette" type="string" value="globalpalette.pal" />
+  </properties>
+  <tile id="0">
+    <properties>
+      <property name="metatile_index" type="int" value="85" />
+      <property name="attribute_index" type="int" value="2" />
+    </properties>
+    <image width="16" height="16" source="chr1_bg2_000_3d_floor_0.png" />
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="metatile_index" type="int" value="86" />
+      <property name="attribute_index" type="int" value="2" />
+    </properties>
+    <image width="16" height="16" source="chr1_bg2_001_3d_floor_0_lrshadow.png" />
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="metatile_index" type="int" value="87" />
+      <property name="attribute_index" type="int" value="2" />
+    </properties>
+    <image width="16" height="16" source="chr1_bg2_002_3d_floor_0_lshadow.png" />
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="metatile_index" type="int" value="88" />
+      <property name="attribute_index" type="int" value="2" />
+    </properties>
+    <image width="16" height="16" source="chr1_bg2_003_3d_floor_0_rshadow.png" />
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="metatile_index" type="int" value="89" />
+      <property name="attribute_index" type="int" value="2" />
+    </properties>
+    <image width="16" height="16" source="chr1_bg2_004_3d_floor_0_tshadow.png" />
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="metatile_index" type="int" value="90" />
+      <property name="attribute_index" type="int" value="2" />
+    </properties>
+    <image width="16" height="16" source="chr1_bg2_005_3d_floor_1.png" />
+  </tile>
+  <tile id="6">
+    <properties>
+      <property name="metatile_index" type="int" value="91" />
+      <property name="attribute_index" type="int" value="2" />
+    </properties>
+    <image width="16" height="16" source="chr1_bg2_006_3d_floor_2.png" />
+  </tile>
+  <tile id="7">
+    <properties>
+      <property name="metatile_index" type="int" value="92" />
+      <property name="attribute_index" type="int" value="2" />
+    </properties>
+    <image width="16" height="16" source="chr1_bg2_007_3d_wall.png" />
+  </tile>
+</tileset>
