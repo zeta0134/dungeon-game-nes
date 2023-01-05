f90c7b945c4892306bc3db63daa329dafa5f6239
Nicholas Flynt <thegamefreak0134@gmail.com>
2023-01-04 19:35:43 -0500

    re-save the entity tileset so it picks up the type->class change

--- art/technical/entities.tsx
+++ art/technical/entities.tsx
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="entities" tilewidth="16" tileheight="16" tilecount="2" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="entities" tilewidth="16" tileheight="16" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="16" height="16" source="../technical/blank.png"/>
+  <image width="16" height="16" source="blank.png"/>
  </tile>
- <tile id="1" type="entity">
+ <tile id="1" class="entity">
   <properties>
    <property name="initial_state" value="blobby_init"/>
   </properties>
-  <image width="16" height="16" source="../technical/entity_blobby.png"/>
+  <image width="16" height="16" source="entity_blobby.png"/>
  </tile>
 </tileset>
