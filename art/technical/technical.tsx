977ef271306867a65273e66d1104c9e9e15e247e
Nicholas Flynt <thegamefreak0134@gmail.com>
2023-05-12 07:55:05 -0400

    Add artwork for a trigger tile

--- art/technical/technical.tsx
+++ art/technical/technical.tsx
@@ -1,37 +1,37 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="technical" tilewidth="16" tileheight="16" tilecount="16" columns="0">
+<tileset version="1.10" tiledversion="1.10.0" name="technical" tilewidth="16" tileheight="16" tilecount="17" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="16" source="blank.png"/>
  </tile>
- <tile id="1" class="exit">
+ <tile id="1" type="exit">
   <image width="16" height="16" source="exit.png"/>
  </tile>
- <tile id="2" class="entrance">
+ <tile id="2" type="entrance">
   <properties>
    <property name="index" type="int" value="1"/>
   </properties>
   <image width="16" height="16" source="entrance_1.png"/>
  </tile>
- <tile id="3" class="entrance">
+ <tile id="3" type="entrance">
   <properties>
    <property name="index" type="int" value="2"/>
   </properties>
   <image width="16" height="16" source="entrance_2.png"/>
  </tile>
- <tile id="4" class="entrance">
+ <tile id="4" type="entrance">
   <properties>
    <property name="index" type="int" value="3"/>
   </properties>
   <image width="16" height="16" source="entrance_3.png"/>
  </tile>
- <tile id="5" class="entrance">
+ <tile id="5" type="entrance">
   <properties>
    <property name="index" type="int" value="4"/>
   </properties>
   <image width="16" height="16" source="entrance_4.png"/>
  </tile>
- <tile id="6" class="entrance">
+ <tile id="6" type="entrance">
   <properties>
    <property name="index" type="int" value="0"/>
   </properties>
@@ -90,5 +90,11 @@
    <property name="collision_variant" type="int" value="9"/>
   </properties>
   <image width="16" height="16" source="shallow_ramp_up_upper.png"/>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="is_trigger" type="bool" value="true"/>
+  </properties>
+  <image width="16" height="16" source="trigger.png"/>
  </tile>
 </tileset>
